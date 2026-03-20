5a7e9dedf531c793abd906512d12b1a492600c16
Taleb El Alem Bella <btalebelale@enseirb-matmeca.fr>
2026-03-20 16:02:39 +0100

    Add tests folder in path

--- scripts/run_valgrind.sh
+++ scripts/run_valgrind.sh
@@ -6,36 +6,36 @@ export LD_LIBRARY_PATH=.
 VALGRIND="valgrind --leak-check=full --show-reachable=yes --track-origins=yes"
 
 echo "===== BASIC ====="
-$VALGRIND ./01-main
-$VALGRIND ./02-switch
-$VALGRIND ./03-equity
+$VALGRIND ./tests/01-main
+$VALGRIND ./tests/02-switch
+$VALGRIND ./tests/03-equity
 
 echo "===== JOIN ====="
-$VALGRIND ./11-join
-$VALGRIND ./12-join-main
-$VALGRIND ./13-join-switch
+$VALGRIND ./tests/11-join
+$VALGRIND ./tests/12-join-main
+$VALGRIND ./tests/13-join-switch
 
 echo "===== CREATE ====="
-$VALGRIND ./21-create-many 100
-$VALGRIND ./22-create-many-recursive 200
-$VALGRIND ./23-create-many-once 100
+$VALGRIND ./tests/21-create-many 100
+$VALGRIND ./tests/22-create-many-recursive 200
+$VALGRIND ./tests/23-create-many-once 100
 
 echo "===== SWITCH ====="
-$VALGRIND ./31-switch-many 20 20
-$VALGRIND ./32-switch-many-join 20 20
-$VALGRIND ./33-switch-many-cascade 10 10
+$VALGRIND ./tests/31-switch-many 20 20
+$VALGRIND ./tests/32-switch-many-join 20 20
+$VALGRIND ./tests/33-switch-many-cascade 10 10
 
 echo "===== FIBO ====="
-$VALGRIND ./51-fibonacci 10
+$VALGRIND ./tests/51-fibonacci 10
 
 echo "===== MUTEX ====="
-$VALGRIND ./61-mutex 20
-$VALGRIND ./62-mutex 20
-$VALGRIND ./63-mutex-equity
-$VALGRIND ./64-mutex-join
+$VALGRIND ./tests/61-mutex 20
+$VALGRIND ./tests/62-mutex 20
+$VALGRIND ./tests/63-mutex-equity
+$VALGRIND ./tests/64-mutex-join
 
 echo "===== PREEMPTION ====="
-$VALGRIND ./71-preemption 20
+$VALGRIND ./tests/71-preemption 20
 
 echo "===== DEADLOCK ====="
-$VALGRIND ./81-deadlock
+$VALGRIND ./tests/81-deadlock
